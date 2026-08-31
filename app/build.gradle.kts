8dbb0c49e4a2a90a6491e1b5d1764d7ac9959a08
ksenia312 <nikitina3619@gmail.com>
2026-08-31 22:22:46 +0200

    feat: version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.xenikii.timecalculator"
         minSdk = 30
         targetSdk = 36
-        versionCode = 22
-        versionName = "4.3"
+        versionCode = 23
+        versionName = "4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
