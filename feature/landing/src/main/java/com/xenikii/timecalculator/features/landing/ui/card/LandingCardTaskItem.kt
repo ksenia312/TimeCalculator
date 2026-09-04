b859e2988ba7cf1b952f93210a5241ff4fa8d68a
Nikitina Kseniia <nikitina3619@gmail.com>
2026-09-04 14:33:05 +0200

    fix: tasks coloring

--- feature/landing/src/main/java/com/xenikii/timecalculator/features/landing/ui/card/LandingCardTaskItem.kt
+++ feature/landing/src/main/java/com/xenikii/timecalculator/features/landing/ui/card/LandingCardTaskItem.kt
@@ -53,7 +53,7 @@ fun LandingCardTaskItem(
     val lightenOverlay = if (isOngoing) {
         Color.Transparent
     } else {
-        MaterialTheme.colorScheme.surface.copy(alpha = 0.28f)
+        MaterialTheme.colorScheme.surface.copy(alpha = 0.40f)
     }
     val textColor = MaterialTheme.colorScheme.surface
     val subTextColor = MaterialTheme.colorScheme.surface.copy(alpha = 0.78f)
